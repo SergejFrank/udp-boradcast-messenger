109b86cee33eb26e6e76f72661bd3815f7da78d9
Valentin <valentin.beller@gmail.com>
2017-09-04 12:15:54 +0200

    fixed message timestamp

--- Models/Message.js
+++ Models/Message.js
@@ -5,8 +5,7 @@ const conf = require('../conf.js')
 function Message(sender,content) {
     this.sender = sender;
     this.content = content;
-    this.timeStamp = 1000
-    //this.timeStamp = Math.floor(Date.now() / 1000);
+    this.timeStamp = Math.floor(Date.now() / 1000);
 }
 
 Message.prototype.getAsBase64 = function(){
