c25036c7bd9578126acd2a05a7132a8cd2b530fb
Jan-Luca Gutsch <janlucagutsch@yahoo.de>
2017-09-04 12:37:37 +0200

    implemented conf.getPublicKey()

--- conf.js
+++ conf.js
@@ -26,6 +26,9 @@ if(localStorage.getItem("uuid") != null){
     localStorage.setItem('uuid', uuid);
 }
 
+function getPublicKey(){
+    return crypto.getPublicKeyFromPrivateKey(key);
+}
 
 exports.key = key;
 exports.port = 34567;
