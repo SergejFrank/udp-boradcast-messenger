cdba18cca563d364e1c6b1dea37f529fbfbf9717
SergejFrank <github@sergej-frank.de>
2017-09-13 14:48:28 +0200

    handle invites

--- Models/Packet.js
+++ Models/Packet.js
@@ -1,10 +1,11 @@
 const uuidV4 = require('uuid/v4');
 
-function Packet(type,chatRoom,content) {
+function Packet(type,chatRoom,content,aesKey) {
     this.id = uuidV4();
     this.type = type;
     this.chatRoom = chatRoom;
     this.content = content;
+    this.aesKey = chatRoom;
 }
 
 module.exports = Packet;
